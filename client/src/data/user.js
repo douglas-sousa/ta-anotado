5e57b89b3465aed84c8f9084ea79455de945e2dd
Douglas Sousa <douglasrdsousa1@gmail.com>
2019-08-30 17:01:03 -0300

    Add files via upload

---
+++ client/src/data/user.js
@@ -0,0 +1,24 @@
+export default null
+
+/*
+A ideia é ter um objeto da pessoa logada, contendo seu nome de usuário e senha,
+e também os ids de referência das anotações feitas por essa pessoa
+
+//Usuário
+{
+  _id: 123456789,
+  username: "Exemplo",
+  password: "1234556789",
+  email: "exemplo@exemplo.com.br"
+}
+
+//Anotação
+{
+  _id: 987654321,
+  title: "Título da anotação",
+  deadline: "Data limite da anotação (opcional)",
+  text: "Conteúdo da anotação",
+  user_id: 123456789
+}
+
+*/
